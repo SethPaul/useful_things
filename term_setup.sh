5e1ba1e973a513918d848af84bff7d54c68ffa5d
SethPaul <sethcpaul@gmail.com>
2019-09-08 10:42:28 -0600

    improve term setup

--- term_setup.sh
+++ term_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 setup_dir="./"
-if [ -z "$setup_dir" ]; then
-    setup_dir=$setup_dir
-else
+if [ -z "$1" ]; then
     setup_dir=$ws
+else
+    setup_dir=$1
 fi
 
 echo "Setting up for wd: $setup_dir"
 gnome-terminal --tab --working-directory $setup_dir 
 xdotool windowsize $(xdotool getwindowfocus) 50% 100% && xdotool windowmove $(xdotool getwindowfocus) 0 0
-gnome-terminal --window --working-directory $setup_dir --tab --working-directory $setup_dir 
-xdotool windowsize $(xdotool getwindowfocus) 50% 100% && xdotool windowmove $(xdotool getwindowfocus) 9999 0
+gnome-terminal --window --working-directory $setup_dir --tab --active --working-directory $setup_dir -- bash -c "xdotool windowsize $(xdotool getwindowfocus) 50% 100% && xdotool windowmove $(xdotool getwindowfocus) 9999 0; exec bash"
